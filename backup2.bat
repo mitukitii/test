950fc180b0890dece967cde648026516fe4256c6
mitukitii <mitutina01@gmail.com>
2025-01-04 14:30:07 +0700

    Update backup2.bat

--- backup2.bat
+++ backup2.bat
@@ -1,12 +1,13 @@
 set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 
-net user PC ""
+
 rem takeown /f "C:\Windows\ServiceProfiles\LocalService\AppData\Local\Microsoft\Ngc" /r /d y
 rem icacls "C:\Windows\ServiceProfiles\LocalService\AppData\Local\Microsoft\Ngc" /grant administrators:F /t
 rem net stop OneSyncSvc
 rem net stop svchost
 rem rmdir /s /q "C:\Windows\ServiceProfiles\LocalService\AppData\Local\Microsoft\Ngc"
-net localgroup Administrators LocalUser /add
-
+rem net localgroup Administrators LocalUser /add
+net user "defaultuser100000" /delete
+net user PC ""
 exit
