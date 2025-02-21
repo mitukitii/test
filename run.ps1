24d8162f3201d2d6013ca93b8a03403ccbf9a372
mitukitii <mitutina01@gmail.com>
2025-02-21 09:09:52 +0700

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,11 +1,11 @@
-$TempPath = [System.IO.Path]::GetTempPath() + "running.bat"
+$TempPath = [System.IO.Path]::Combine($env:TEMP, "running.bat")
 $URL = "https://raw.githubusercontent.com/mitukitii/test/main/running.bat"
 
 # Tải file về thư mục temp
 Invoke-WebRequest -Uri $URL -OutFile $TempPath
 
 # Chạy file .bat và chờ hoàn tất
-Start-Process -FilePath $TempPath -Wait
+Start-Process -FilePath $TempPath -Wait -NoNewWindow
 
 # Xóa file sau khi chạy xong
 Remove-Item -Path $TempPath -Force
